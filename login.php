a1aa7863cca9229d22f3c0d77271ae84a6bc21bb
Joseph Beasse <94929813+Zoko91@users.noreply.github.com>
2022-04-22 11:47:49 +0200

    Update login.php

--- login.php
+++ login.php
@@ -8,23 +8,20 @@
 <!-- Ne pas pouvoir de rendre sur cette page si déjà connecté -->
 <div class="text-center text-white"><h2>Connexion</h2></div>
 
-<div class="container mx-auto">
-    <form class="px-4 py-3">
+<div class="container mx-auto text-white" id="containerConnexion" style="width:30%;">
+    <form class="px-4 py-3" method="post" action="scripts/script_connexion.php">
         <div class="mb-3">
             <label for="FormUsername" class="form-label">Username</label>
             <input type="text" name="usrName" class="form-control" id="FormUsername" placeholder="Username">
         </div>
         <div class="mb-3">
-            <label for="exampleDropdownFormPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password">
+            <label for="FormPassword" class="form-label">Password</label>
+            <input type="password" name="usrPass" class="form-control" id="FormPassword" placeholder="Password">
         </div>
         <div class="dropdown-divider"></div>
         <button type="submit" class="btn btn-primary">Sign in</button>
         <a type="button" class="btn btn-danger" href="inscription.php">Sign up</a>
     </form>
-    <div class="dropdown-divider"></div>
-    <a class="dropdown-item" href="#">New around here? Sign up</a>
-    <a class="dropdown-item" href="#">Forgot password?</a>
 </div>
 
 
