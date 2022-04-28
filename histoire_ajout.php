c2ac3b688a134c3900a197a86c63bd71bd4b5fcb
tristangclvs <goncalvestristan.pro@pm.me>
2022-04-28 16:47:12 +0200

    Changes by User

--- histoire_ajout.php
+++ histoire_ajout.php
@@ -1,7 +1,58 @@
 <?php include("entete.php") ?>
+<?php
+if (isset($_SESSION["doublon_histoire"])){
+    if ($_SESSION["doublon_histoire"]) { ?>
+        <div class="alert alert-danger text-center"><span style="font-weight: bold">Erreur ! </span>L'histoire existe déjà ou le titre correspond à une histoire déjà existante.</div><br>
+        <?php
+        $_SESSION["doublon_histoire"] = false;
+    }
+}
+?>
+<!-- Si il n'est pas connecté, il ne peut accéder a cette page-->
 
+<div class="text-center"><h2>Ajout d'une histoire :</h2></div>
 
+<br>
+<div class="container mx-auto" id="containerConnexion" style="width:30%;">
+    <fieldset class="border border-light border-2 rounded">
+        <form class="px-4 py-3" method="post" action="scripts/script_ajoutHistoire.php">
+            <div class="mb-3">
+                <label for="FormTitre" class="form-label">Titre</label>
+                <input type="text" name="titre" class="form-control" id="FormTitre" placeholder="Titre" required>
+            </div>
+            <div class="mb-3 form-group">
+                <label for="choixCategories" class="form-label">Catégorie de l'histoire</label>
+                <select name="categorie" id="choixCategories" class="form-control text-dark">
+                    <option value="horreur">Horreur</option>
+                    <option value="romantique">Romantique</option>
+                    <option value="action">Action</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="TextAuteur" class="form-label">Auteur</label>
+                <input type="text" name="auteur" class="form-control" id="TextAuteur" placeholder="Auteur" value="<?=$_SESSION["nomUtilisateur"]?>">
+            </div>
+            <div class="mb-3">
+                <label for="TextAnnee" class="form-label">Année de parution</label>
+                <input type="number" name="annee" class="form-control" id="TextAnnee" placeholder="Annee"  required>
+            </div>
+            <div class="mb-3">
+                <label for="TextDescription" class="form-label">Description</label>
+                <textarea type="text" name="description" class="form-control" id="TextDescription" required></textarea>
+            </div>
+            <div class="mb-3 ">
+                <label for="TextImage" class="form-label custom-file-label">Image <small>(facultatif)</small></label>
+                <input type="file" name="image" class="form-control custom-file-input" id="TextImage">
+            </div>
 
+            <div class="dropdown-divider bg-light mb-3"></div>
+
+            <button type="submit" class="btn text-white" id="button" aria-describedby="poursuivre">Ajout de l'histoire</button>
+            <small id="poursuivre" class="text-muted text_white" >
+                Poursuivre l'ajout de l'histoire ... </small>
+        </form>
+    </fieldset>
+</div>
 
 
 
